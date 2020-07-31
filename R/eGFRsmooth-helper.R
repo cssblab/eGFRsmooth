d01ed8aafe1accd0100f856c1c178e73a6b7880e
Joseph Lee <joseph.lee@u.nus.edu>
2020-08-01 04:07:58 +0800

    add R code

---
+++ R/eGFRsmooth-helper.R
@@ -0,0 +1,162 @@
+# Helper function - calculate average slope
+avgSlope = function(y, w) {
+    avg = 0
+    ## Calculate avg slope in the window, each interval is 10 days
+    for (i in 2:(w+1)) {
+        avg = avg + (y[i] - y[i-1]) / 10
+    }
+    return(avg/w * 365)
+}
+
+# Helper function - trim values for regions with dense data points
+trimValues <- function(x, y, thres.acute) {
+    z1 = x[-1] - x[-length(x)]
+    z2 = z1[-1]
+    z1 = z1[-length(z1)]
+    rid = which(z1 < thres.acute & z2 < thres.acute)
+    if (length(rid) != 0) {
+        nx = x[-(rid+1)]
+        ny = y[-(rid+1)]
+        rx = x[(rid+1)]
+        ry = y[(rid+1)]
+        rx = rx - x[1]
+        nx = nx - x[1]
+    } else {
+        nx = x - x[1]
+        ny = y
+        rx = NULL
+        ry = NULL
+    }
+    return(list(nx, ny, rx, ry))
+}
+
+# Plotting function - smooth single trajectory
+plotSmooth = function(x, y, rx, ry, eGFR.range,
+                      k.width, w.width, min.width, max.range, name = NULL) {
+    ## Initialize variables
+    min = NA
+    perc.drop = NA
+    s.egfr = NA
+    e.egfr = NA
+    numpts = NA
+    lead = NA
+    
+    ## Read input, plot the points
+    id = which(!is.na(x) & !is.na(y))
+    x = x[id]  ## Time point of non-missing eGFR measurements
+    y = y[id]  ## Corresponding eGFR values
+    rg.x = range(x)
+    plot(0, type="b", pch= 19, xlab="Time", ylab="eGFR",
+         xlim=rg.x, ylim= eGFR.range, yaxt = "n",
+         cex=.5, lty = 3, col = "white")
+    axis(2, at = seq(0, round(eGFR.range[2],-1), by = 20),
+         labels = as.character(seq(0, round(eGFR.range[2], -1), by = 20)),
+         cex.axis = 0.7, las = 1)
+    points(rx, ry, col = "gray70", pch = 19, cex = .5)
+    points(x, y, pch = 19, cex = 0.5, lty = 3, type = "b")
+    
+    ## Set the time points - split one year into approx 36 intervals
+    xx = seq(rg.x[1], rg.x[2], by = 10)
+    yest = rep(NA, length(xx))
+    n = length(xx)
+    
+    ## If the range is greater than a min.width, smooth it
+    if (length(x) >= 4 & diff(range(x)) > min.width) {
+        for(j in 1:n) {
+            ## Gets distribution of weights
+            ## Smoothing window size as st.dev
+            ww = dnorm(x, xx[j], k.width)  
+            ## Weighted eGFR value
+            yest[j] = sum(ww * y) / sum(ww)
+        }
+        
+        ## For large intervals in time, draw a straight line
+        for(k in 1:(length(x)-1)) {
+            tmp.d = x[k+1] - x[k]
+            if(tmp.d > 365) {
+                wid = which(xx >= x[k] & xx <= x[k+1])
+                y1 = yest[xx == min(xx[wid])]
+                y2 = yest[xx == max(xx[wid])]
+                
+                dst1 = abs(xx - x[k])
+                dst2 = abs(xx - x[k+1])
+                
+                new.yest = y1 * (dst2 / (dst1 + dst2)) + y2 * (dst1 / (dst1 + dst2))
+                yest[wid] = new.yest[wid]
+                
+            }
+        }
+        points(xx, yest, type = "l")
+        
+        ## Sliding window - variable sliding window - perform only if range is greater than window width
+        if (diff(range(x)) > w.width) {
+            ## Round window to the nearest 10
+            w.width = round(w.width, -1)
+            w = w.width / 10
+            min = 1000 ## Arbitrary large number
+            start = 1 ## In the case that the first window is the window of interest
+            for (i in 1:(length(yest) - w)) {
+                ## We break the loop once the end point of the range we're 
+                ## calculating is greater than max range
+                if (!is.na(max.range)) {
+                    if (xx[i+w] > max.range) {
+                        break
+                    }
+                }
+                tmp = avgSlope(yest[i:(i+w)], w)
+                if (tmp < min) {
+                    min = tmp
+                    start = i
+                }
+            }
+            ## Highlight in red the range with the steepest avg slope
+            points(xx[start:(start+w)], yest[start:(start+w)], type = "l", col = 2, lwd = 1.5)
+            if (!is.na(max.range)) {
+                abline(v = max.range, lwd = 0.5, col = 4, lty = 3)
+            }
+            
+            ## Record the starting eGFR and ending eGFR of the window used
+            s.egfr = yest[start]
+            e.egfr = yest[start+w]
+            numpts = sum(x >= xx[start] & x <= xx[start+w])
+            lead = xx[start] - x[1]
+            ## Calculate the % drop
+            perc.drop = (e.egfr - s.egfr) / s.egfr * 100
+            ## Add info to plot
+            par(cex.main = 0.8)
+            title(main = paste(name, "\n", "Steepest Avg Slope = ", round(min,1), 
+                               "\n", "Percentage Change = ", round(perc.drop,1), "%"))
+        }
+        ## If the range is smaller than window width but greater than a minimum 
+        ## width (say, 1 year), calculate using the whole range of the data
+        else if (diff(range(x)) > min.width) {
+            min = avgSlope(yest, length(xx)-1)
+            s.egfr = yest[1]
+            e.egfr = yest[length(yest)]
+            numpts = length(x)
+            lead = 0
+            ## Calculate the % drop
+            perc.drop = (e.egfr - s.egfr) / s.egfr * 100
+            ## Add info to plot
+            par(cex.main = 0.8)
+            title(main = paste(name, "\n", "Avg Slope = ", round(min,1),
+                               "\n", "Percentage Change = ", round(perc.drop,1), "%"))
+        }
+        ## If the range is smaller than window width and smaller than minimum width, don't calculate anything
+        else {
+            title(main = name)
+        }
+    }
+    ## Less than 4 points and range less than a year - not smoothed
+    else {
+        title(main = name)
+    }
+    
+    ## Return the slope, percentage change, starting  and ending egfr in the window - NA if not calculated
+    ## And the baseline eGFR readings, number of observations, and the time period of the follow up
+    list(Slope = min, Percentage.Drop = perc.drop,
+         eGFR.Window.Start = s.egfr, eGFR.Window.End = e.egfr,
+         eGFR.Baseline = y[1], Num.Observations = length(x), 
+         Time.Range.Followup = diff(range(x)),
+         Num.Pts = numpts, Lead.Time = lead)
+}
---
+++ R/eGFRsmooth.R
@@ -0,0 +1,134 @@
+#' Smooths eGFR trajectories and outputs plots for each subject, and a table
+#' summarizing slope and percentage change for each trajectory
+#' 
+#' @param data A data.frame. Input data with subject ID, numeric date and eGFR  
+#' @param k.width A numeric value. Smoothing bandwidth parameter
+#' @param w.width A numeric value. Width of window (in years). Defaults to 
+#'   2 years
+#' @param min.width A numeric value. Minimal width (in years) for a trajectory 
+#'   to be smoothed. Defaults to 1 year
+#' @param thres.acute A numeric value. This argument defines the minimum width 
+#'   a data point must be away from both of it's neighbours to be kept. 
+#'   Otherwise, the data point is trimmed. This removes dense clusters of points
+#'   usually characterized by large variance. Defaults to 30 days
+#' @param max.range A numeric value. The maximum range (in years) from the
+#'   baseline date to be analyzed. Defaults to NA (unrestricted range)
+#' @param sort.param A character vector. Controls output plot name. One of 
+#'   Slope, Percentage.Drop, eGFR.Window.Start, eGFR.Window.End, 
+#'   eGFR.Baseline, Num.Observations, Time.Range.Followup, Num.Pts, Lead.Time.
+#'   Defaults to Percentage.Drop
+#' @param decreasing A boolean value. Controls sorting
+#' @param output.dir A character value. Output directory for plots and 
+#'   summary table. Defaults to NULL
+#' 
+#' @export
+#' @return A data.frame. Summarizes the slope, percentage change and other 
+#'  statistics for each trajectory. Outputs a plot (pdf) and table (txt) to 
+#'  present working directory 
+#'  
+eGFRsmooth <- function(data, k.width, w.width = 2, min.width = 1, 
+                       thres.acute = 30, max.range = NA, output.dir = NULL,
+                       sort.param = 'Percentage.Drop', decreasing = FALSE) {
+  print('Generating unsorted eGFR statistics and plots...')
+  result <- drawPlots(data = data,
+                      k.width = k.width, w.width = w.width, min.width = min.width,
+                      thres.acute = thres.acute, max.range = max.range,
+                      output.dir = output.dir)
+  print('Generating sorted plots...')
+  drawPlotsSorted(data = data, result = result,
+                  k.width = k.width, w.width = w.width, min.width = min.width,
+                  thres.acute = thres.acute, max.range = max.range,
+                  sort.param = sort.param, decreasing = decreasing,
+                  output.dir = output.dir)
+}
+
+#' @importFrom dplyr as_tibble '%>%' filter arrange group_by summarise mutate
+#' @importFrom tidyr unnest pivot_wider
+#' @importFrom rlang .data
+drawPlots = function(data, k.width, w.width, min.width, 
+                     thres.acute, max.range, 
+                     table = TRUE, sorted = FALSE, output.dir) {
+  if(is.null(output.dir)){
+    output.dir <- getwd()
+  }
+  output.dir <- paste0(output.dir, '/output')
+  if(!dir.exists(output.dir)){
+    dir.create(output.dir, recursive = TRUE)
+  }
+  
+  w.width <- 365 * w.width
+  min.width <- 365 * min.width
+  max.range <- 365 * max.range
+  
+  if (sorted) {
+    pdf(paste0(output.dir, '/sorted.pdf'), height = 8, width = 7)
+  } else {
+    pdf(paste0(output.dir, '/unsorted.pdf'), height = 8, width = 7)
+  }
+  par(mfrow=c(3,3))
+  
+  result <- as_tibble(data) %>%
+    dplyr::filter(!is.na(.data$eGFR)) %>%
+    dplyr::arrange(.data$Subject, .data$Date) %>%
+    dplyr::group_by(.data$Subject) %>%
+    dplyr::summarise(args=trimValues(.data$Date, .data$eGFR, thres.acute), 
+                     .groups='drop') %>%
+    dplyr::group_by(.data$Subject) %>%
+    dplyr::summarise(res=plotSmooth(x = unlist(nth(args, 1)),
+                             y = unlist(nth(args, 2)),
+                             rx = unlist(nth(args, 3)),
+                             ry = unlist(nth(args, 4)),
+                             eGFR.range = range(data$eGFR),
+                             k.width = k.width,
+                             w.width = w.width,
+                             min.width = min.width,
+                             max.range = max.range,
+                             name = unique(.data$Subject)),
+                    .groups='drop') %>%
+    tidyr::unnest(., res) %>%
+    dplyr::mutate(ID=rep(c('Slope', 
+                    'Percentage.Drop', 
+                    'eGFR.Window.Start',
+                    'eGFR.Window.End',
+                    'eGFR.Baseline',
+                    'Num.Observations',
+                    'Time.Range.Followup',
+                    'Num.Pts',
+                    'Lead.Time'), length(unique(.data$Subject)))) %>%
+    tidyr::pivot_wider(., names_from = ID, values_from = res)
+  dev.off()
+  if (table) {
+    print('Generating results table...')
+    write.table(result, paste0(output.dir, '/table.txt'), 
+                quote = FALSE, sep = "\t", row.names = FALSE)
+    return(result)
+  }
+}
+
+#' @importFrom dplyr '%>%' filter arrange
+#' @importFrom rlang .data
+drawPlotsSorted = function(data, result, k.width, w.width, min.width, 
+                           thres.acute, max.range, sort.param, 
+                           decreasing, output.dir) {
+  sort.id <- grep(sort.param, names(result), ignore.case = TRUE)
+  if (length(sort.id) == 0) {
+    stop('Invalid sorting parameter. See ?eGFRsmooth for valid arguments')
+  }
+  
+  result <- result %>% 
+    dplyr::filter(!is.na(.data$Slope)) %>%
+    dplyr::arrange(., .[[sort.id]])
+    
+  subject.order <- as.factor(result$Subject)
+  if (decreasing) {
+    subject.order <- rev(subject.order)
+  }
+  
+  ## Sort data
+  data[,1] = factor(data[,1], levels = subject.order)
+  data = data[order(data[,1]),]
+
+  ## Call drawPlots, sorted = T for pdf name
+  drawPlots(data, k.width, w.width, min.width, thres.acute, max.range, 
+            table = FALSE, sorted = TRUE, output.dir)
+}
